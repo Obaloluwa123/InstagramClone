295a03cd9988bdab035183b857bcbed09251527a
David Olaniran <davidolaniran@fb.com>
2022-06-14 17:21:01 -0700

    Initial commit

---
+++ app/src/main/java/com/codepath/oobal/Instagram/ParseApplication.java
@@ -0,0 +1,23 @@
+package com.codepath.oobal.Instagram;
+
+import android.app.Application;
+
+import com.codepath.oobal.Instagram.data.model.Post;
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("zc1YmfY67zxPCkmaga3wS6BSUhiyqcovAOmTdHhW")
+                .clientKey("1YMCY2bKWB70I9QVzeNSftW12yodtirMcAPFstde")
+                .server("https://parseapi.back4app.com")
+                .build()
+
+        );
+    }
+}
---
+++ app/src/main/java/com/codepath/oobal/Instagram/PostFragment.java
@@ -0,0 +1,26 @@
+package com.codepath.oobal.Instagram;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class PostFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
